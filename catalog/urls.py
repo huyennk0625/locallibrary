a9d05b4cea430e91a31f00fa0ab669b6fc0fa97e
huyennk <huyen16102004@gmail.com>
2025-07-11 15:01:42 +0700

    Django Tutorial Part 5: Creating our home page

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # Trang chủ của app "catalog"
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
 
